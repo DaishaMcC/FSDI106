a1916b0480a43aa6fb536fbb0d07e4eb2e31ae28
DaishaMcC <daisha05311994@gmail.com>
2024-05-08 02:24:23 -0400

    106

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+function saveTask(){
+    console.log("saving task");
+    //get values
+    const title =$("#txtTitle").val();
+    const desc =$("#txtDescription").val();
+    const color =$("#selColor").val();
+    const date =$("#selDate").val();
+    const status =$("#selStatus").val();
+    const budget =$("#numBudget").val();
+    console.log(title,desc,color,date,status,budget)
+    //build an object
+
+    //save to server
+
+    //display the task
+
+}
+
+function init()
+{
+    //load data
+
+    //hook events
+    $("#btnSave").click(saveTask)
+}
+
+window.onload = init;//avoid the init()
